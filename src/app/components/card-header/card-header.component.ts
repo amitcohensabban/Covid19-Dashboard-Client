cb6088724a9ada5ac5baf46a3432c236099b81ce
AMIT COHEN SABBAN <amitcohensaban@gmail.com>
2023-06-06 14:47:39 +0300

    hover-info

--- src/app/components/card-header/card-header.component.ts
+++ src/app/components/card-header/card-header.component.ts
@@ -10,5 +10,6 @@ export class CardHeaderComponent {
   @Input() openWindowsIIcon:string="";
   @Input() isDarkModeActive?:boolean;
   @Input()id:string=""
-
+  showTooltip: boolean = false;
+  tooltipText: string = "מספר החולים הפעילים במחלת הנגיף COVID-19, נכון למועד העדכון האחרון. חולים פעילים - מי שנבדקו ונמצאו חיוביים לנגיף בבדיקת PCR או בבדיקת אנטיגן מפוקחת  COVID-19, ללא קשר להופעת תסמינים, שעוד לא הוגדרו כמחלימים ושלא נפטרו";
 }
