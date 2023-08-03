662966f4a1a027cf19c3d74ccc7e9c82c02eec6b
AMIT COHEN SABBAN <amitcohensaban@gmail.com>
2023-08-03 18:28:57 +0300

    Implement sorting functionality for tables

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges  } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+} from '@angular/core';
 import { tables } from 'src/app/data/app.anchorList';
 import { FilterService } from 'src/app/services/filter.service';
 @Component({
@@ -11,13 +15,14 @@ export class TableComponent implements OnInit {
   @Input() tableName!: string;
   filteredTableData!: any;
   tableNumbers!: any[];
-  counter: number = 0;
+  sortedColumn: string = '';
+  sortAscending: boolean = true;
   constructor(private filterService: FilterService) {}
 
   ngOnInit(): void {
     this.tableNumbers = tables;
     console.log(this.tableName);
-console.log(this.tableData);
+    console.log(this.tableData);
 
     this.filterService.setInitialData(this.tableData, this.tableName);
     this.filterService
@@ -25,5 +30,25 @@ console.log(this.tableData);
       .subscribe((filteredData) => {
         this.filteredTableData = filteredData;
       });
+  }
+
+  sortTable(columnName: string) {
+    // If clicking the same column, toggle the sorting order
+    if (this.sortedColumn === columnName) {
+      this.sortAscending = !this.sortAscending;
+    } else {
+      // If clicking a different column, set the new column and reset the sorting order
+      this.sortedColumn = columnName;
+      this.sortAscending = true;
+    }
+
+    // Sort the table data based on the selected column and order
+    this.filteredTableData.sort((a: any, b: any) => {
+      if (this.sortAscending) {
+        return a[columnName] > b[columnName] ? 1 : -1;
+      } else {
+        return a[columnName] < b[columnName] ? 1 : -1;
+      }
+    });
   }
 }
